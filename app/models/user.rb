cae4e2297e4b1047430e306900c9f843f3b655a8
nmhummel <natalie.hummel@gmail.com>
2020-12-30 23:36:28 -0600

    commit tree 42fc80bd796bde5be5a73fd29bcbbd62b148f0ac

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
-  has_secure_password
   has_many :tweets
+  has_secure_password
+  validates :username, presence: true, uniqueness: { message: "%{value} is already in use.  Please select another or login."}
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, presence: true, uniqueness: { message: "%{value} is already in use.  Please select another or login."}
+
+  def slug
+    username.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
+  
 end
